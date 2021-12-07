75d2139c7a7c0f554f59b27a02ae916df1ac9a44
Henrique-BO <henrique.barrosoliveira@usp.br>
2021-12-07 20:55:03 -0300

    Implementar view de cardápio e listagem de pedidos

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'pedidos'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('pedidos', views.list_pedidos, name='pedidos'),
+    path('cardapio', views.cardapio, name='cardapio'),
 ]
--- pedidos/views.py
+++ pedidos/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 
+from accounts.models import ChefCozinha
+
+from .models import Pedido, Item
 
 def index(request):
     context = {}
     return render(request,'pedidos/index.html', context)
+
+def cardapio(request):
+    item_list = Item.objects.filter(disponivel=True)
+    context = {'item_list': item_list}
+    return render(request, 'pedidos/cardapio.html', context)
+
+def list_pedidos(request):
+    pedidos_realizados = Pedido.objects.filter(chefcozinha=None)
+    pedidos_preparacao = Pedido.objects.filter(pronto=False).exclude(chefcozinha=None)
+    pedidos_prontos = Pedido.objects.filter(pronto=True).exclude(chefcozinha=None).filter(garcom=None)
+    context = {
+        'pedidos_realizados': pedidos_realizados,
+        'pedidos_preparacao': pedidos_preparacao,
+        'pedidos_prontos': pedidos_prontos,
+    }
+    return render(request, 'pedidos/pedidos.html', context)
