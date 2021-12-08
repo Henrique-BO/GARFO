b82357b3351190b17b1924d1be61ea287d0a1d5c
Henrique-BO <henrique.barrosoliveira@usp.br>
2021-12-08 17:23:38 -0300

    Implementar atualização e deleção de itens

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('cardapio/', views.cardapio, name='cardapio'),
     path('cardapio/<int:item_id>/', views.detail_item, name='detail_item'),
     path('cardapio/create/', views.create_item, name='create_item'),
+    path('cardapio/<int:item_id>/update/', views.update_item, name='update_item'),
+    path('cardapio/<int:item_id>/delete/', views.delete_item, name='delete_item'),
 ]
--- pedidos/views.py
+++ pedidos/views.py
@@ -15,11 +15,6 @@ def cardapio(request):
     context = {'item_list': item_list}
     return render(request, 'pedidos/cardapio.html', context)
 
-def detail_item(request, item_id):
-    item = get_object_or_404(Item, pk=item_id)
-    context = {'item': item}
-    return render(request, 'pedidos/detail_item.html', context)
-
 def create_item(request):
     if request.method == 'POST':
         item_form = ItemForm(request.POST)
@@ -33,6 +28,46 @@ def create_item(request):
         item_form = ItemForm()
     context = {'item_form': item_form}
     return render(request, 'pedidos/create_item.html', context)
+
+def detail_item(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    context = {'item': item}
+    return render(request, 'pedidos/detail_item.html', context)
+
+def update_item(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    if request.method == 'POST':
+        item_form = ItemForm(request.POST)
+        if item_form.is_valid():
+            item.nome = item_form.cleaned_data['nome']
+            item.preco = item_form.cleaned_data['preco']
+            item.descricao = item_form.cleaned_data['descricao']
+            item.foto_url = item_form.cleaned_data['foto_url']
+            item.disponivel = item_form.cleaned_data['disponivel']
+            item.save()
+            return HttpResponseRedirect(
+                reverse('pedidos:detail_item', args=(item.pk, ))
+            )
+    else:
+        item_form = ItemForm(
+            initial={
+                'nome': item.nome,
+                'preco': item.preco,
+                'descricao': item.descricao,
+                'foto_url': item.foto_url,
+                'disponivel': item.disponivel,
+            }
+        )
+    context = {'item_form': item_form, 'item': item}
+    return render(request, 'pedidos/update_item.html', context)
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    if request.method == 'POST':
+        item.delete()
+        return HttpResponseRedirect(reverse('pedidos:index'))
+    context = {'item': item}
+    return render(request, 'pedidos/delete_item.html', context)
 
 def list_pedidos(request):
     pedidos_realizados = Pedido.objects.filter(chefcozinha=None)
