f2e2c47ecc3a912154e219c457d8e00e60f62820
Julia Frejat <juliafrejat@usp.br>
2021-12-09 22:03:58 -0300

    Criar páginas de cadastro do cliente

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,17 +1,36 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from django.forms import fields
+from accounts.models import Cliente, Gerente, Garcom, ChefCozinha
 
-class UserLoginForm(AuthenticationForm):
-   def __init__(self, * args, ** kwargs): super(UserLoginForm, self).__init__( * args, ** kwargs)
+class TeamUserLoginForm(AuthenticationForm):
+   def __init__(self, * args, ** kwargs): super(TeamUserLoginForm, self).__init__( * args, ** kwargs)
 
-username = forms.EmailField(widget = forms.TextInput(
-   attrs = {
-      'class': 'form-control',
-      'placeholder': 'Nome',
-   }))
-password = forms.CharField(widget = forms.PasswordInput(
-   attrs = {
-      'class': 'form-control',
-      'placeholder': 'Senha',
-   }
-))
+   username = forms.CharField(widget = forms.TextInput(
+      attrs = {
+         'class': 'form-control',
+         'placeholder': 'Nome',
+      }))
+   password = forms.CharField(widget = forms.PasswordInput(
+      attrs = {
+         'class': 'form-control',
+         'placeholder': 'Senha',
+      }))
+
+class ClientCreationForm(forms.ModelForm):
+   class Meta:
+      model = Cliente
+      fields = [
+            'cpf',
+            'mesa',
+        ]
+      labels = {
+         'cpf': 'CPF',
+         'mesa': 'Mesa',
+      }
+      widgets = {
+            'cpf': forms.TextInput(attrs={
+               'class': 'form-control', 
+               'placeholder': 'CPF'
+      })}
