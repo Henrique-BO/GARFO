afc165db0cde07b59e9f2de00038afac31301dd7
Julia Frejat <juliafrejat@usp.br>
2021-12-12 19:22:12 -0300

    Criar páginas de cadastro e login de cliente e atualizar login de equipe

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,11 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UsernameField
 from django.contrib.auth.models import User
 from django.forms import fields
 from accounts.models import Cliente, Gerente, Garcom, ChefCozinha
 
-class TeamUserLoginForm(AuthenticationForm):
-   def __init__(self, * args, ** kwargs): super(TeamUserLoginForm, self).__init__( * args, ** kwargs)
+class UserLoginForm(AuthenticationForm):
+   def __init__(self, * args, ** kwargs): super(UserLoginForm, self).__init__( * args, ** kwargs)
 
    username = forms.CharField(widget = forms.TextInput(
       attrs = {
@@ -34,3 +34,32 @@ class ClientCreationForm(forms.ModelForm):
                'class': 'form-control', 
                'placeholder': 'CPF'
       })}
+
+class ClientUserCreationForm(UserCreationForm):
+   def __init__(self, * args, ** kwargs): super(UserCreationForm, self).__init__( * args, ** kwargs)
+
+   username = forms.CharField(widget = forms.TextInput(
+      attrs = {
+         'class': 'form-control',
+         'placeholder': 'Nome',
+      }))
+   password1 = forms.CharField(widget = forms.PasswordInput(
+      attrs = {
+         'class': 'form-control',
+         'placeholder': 'Senha',
+      }))
+   password2 = forms.CharField(widget = forms.PasswordInput(
+      attrs = {
+         'class': 'form-control',
+         'placeholder': 'Repita a senha',
+      }))
+
+class ClientMesaForm(forms.ModelForm):
+   class Meta:
+      model = Cliente
+      fields = [
+            'mesa',
+        ]
+      labels = {
+         'mesa': 'Mesa',
+      }
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,15 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
-from accounts.forms import ClientCreationForm
+from accounts.forms import ClientCreationForm, ClientUserCreationForm, UserLoginForm, ClientMesaForm
 from accounts.models import Cliente
 
 def client_signup_user(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = ClientUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             new_user = authenticate(username=form.cleaned_data['username'],
@@ -17,7 +18,7 @@ def client_signup_user(request):
             login(request, new_user)
             return HttpResponseRedirect(reverse('index'))
     else:
-        form = UserCreationForm()
+        form = ClientUserCreationForm()
 
     context = {'form': form}
     return render(request, 'accounts/client_signup_user.html', context)
@@ -42,12 +43,24 @@ def client_signup(request):
 
 def client_login(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserLoginForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('index'))
+            return HttpResponseRedirect(reverse('accounts:client_login_mesa'))
     else:
-        form = UserCreationForm()
+        form = UserLoginForm()
+
+    context = {'form': form}
+    return render(request, 'accounts/client_login.html', context)
+
+def client_login_mesa(request):
+    if request.method == 'POST':
+        form = ClientMesaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('pedidos:cardapio'))
+    else:
+        form = ClientMesaForm()
 
     context = {'form': form}
     return render(request, 'accounts/client_login.html', context)
